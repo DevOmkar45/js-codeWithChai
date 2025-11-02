6d04a0e47ff3d91612b66a652989bf10c74d437c
DevOmkar45 <omkar.mendhe05@gmail.com>
2025-11-02 11:12:46 +0000

    datatype

---
+++ 01_baisc/01_dataType.js
@@ -0,0 +1,8 @@
+let name = "omakr"  //string
+let age = 20 //number
+let isabc = true  //boolean 
+
+//undefined => variable which is not initialized 
+let accoundName;
+//null => it is one datatype mean empty 
+//object => non premitive data type
---
+++ 01_baisc/01_variables.js
@@ -0,0 +1,7 @@
+const number = 23;
+let name = "omkar";
+
+let password;
+number = 24;
+
+console.table([number, name, password]);
